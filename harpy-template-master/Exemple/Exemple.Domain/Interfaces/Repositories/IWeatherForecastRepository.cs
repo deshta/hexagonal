a219dc8bb2652255def6501cb6a97ad126ff6ba0
Guilherme <guimn1990@gmail.com>
2020-06-05 09:38:15 -0300

    alteração na estrutura

--- harpy-template-master/Exemple/Exemple.Domain/Interfaces/Repositories/IWeatherForecastRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿using Exemple.Domain.AggregateModels;
-using Harpy.Domain.Interfaces.Repositories.Entity;
-
-namespace Exemple.Domain.Interfaces.Repositories {
-
-    public interface IWeatherForecastRepository: IEntityReadWriteRepository<WeatherForecast> {
-    }
-}
